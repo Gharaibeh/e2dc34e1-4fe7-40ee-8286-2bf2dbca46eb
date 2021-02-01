45c430820747bc19a58442bcfb8ab9da82fef2a7
Hasan Gharaibeh <swehu1988@gmail.com>
2021-02-01 22:14:44 +1100

    Applied cohesion

---
+++ V1/V1/Sequence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V1
+{
+    class Sequence
+    {
+        string[] array;
+        List<string> myList;
+        List<string> resultList1;
+        List<string> resultList2;
+
+        public Sequence()
+        {
+            Console.WriteLine("Constructor");
+            myList = new List<string>();
+            resultList1 = new List<string>();
+            resultList2 = new List<string>();
+            fun1("6 1 5 9 2");
+
+           
+        }
+        public void print()
+        {
+            Console.WriteLine("Testing Class");
+        }
+    }
+}
